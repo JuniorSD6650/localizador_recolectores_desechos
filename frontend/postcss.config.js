c14843c252b85140918d488d471e9ec6321fa548
Arvic Kenedi <arvic2514@gmail.com>
2025-01-31 16:32:55 -0500

    chore: Cambiar color de fondo de CardAnimation a verde

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,7 @@
 // filepath: /c:/Users/TurismoProyect/Documents/GatilinDigital/be_recolectores_desechos/frontend/postcss.config.cjs
-module.exports = {
+export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-};
+}
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-white py-6">
+        <footer style={{ backgroundColor: '#595957' }} className="text-white py-6">
             <div className="container mx-auto text-center">
                 <p className="text-sm">
                     &copy; {new Date().getFullYear()} Sistema Recolector. Todos los derechos reservados.
