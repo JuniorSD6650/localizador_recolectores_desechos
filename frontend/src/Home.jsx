9e47fc1735d6554cf68bc5949f3f28eee491dc71
JuniorSD6650 <fancy6650@gmail.com>
2025-01-31 10:05:08 -0500

    chore: Logo en Nav - Juntar Nav

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -37,7 +37,6 @@ const Home = () => {
                     />
                 </div>
             </div>
-            <img src="/images/logo_amarilis.png" alt="Logo Amarilis" className="mx-auto mb-4" style={{ width: '390px', height: 'auto' }} />
         </div>
     );
 };
--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -29,11 +29,29 @@ const Navbar = () => {
         <div>
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-                    <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse no-underline">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Sistema Recolector</span>
+                    <a href="/" className="flex items-center justify-center space-x-3 rtl:space-x-reverse no-underline">
+                        <img src="/images/logo_amarilis.png" alt="Logo Amarilis" className="mx-auto mb-4" style={{ width: '250px', height: 'auto' }} />
                     </a>
                     <div className="flex items-center space-x-6 rtl:space-x-reverse">
+                        <Link
+                            to="/"
+                            className={`text-gray-900 dark:text-white no-underline ${location.pathname === '/' ? 'font-bold' : ''}`}
+                            aria-current="page"
+                        >
+                            Inicio
+                        </Link>
+                        <Link
+                            to="/reportar"
+                            className={`text-gray-900 dark:text-white no-underline ${location.pathname === '/reportar' ? 'font-bold' : ''}`}
+                        >
+                            Reportar
+                        </Link>
+                        <Link
+                            to="/recolectores"
+                            className={`text-gray-900 dark:text-white no-underline ${location.pathname === '/recolectores' ? 'font-bold' : ''}`}
+                        >
+                            Recolectores
+                        </Link>
                         <div className="relative" ref={notificationsRef}>
                             <button onClick={toggleNotifications} className="text-gray-500 dark:text-white focus:outline-none">
                                 <NotificationsIcon />
@@ -48,42 +66,9 @@ const Navbar = () => {
                                 </div>
                             )}
                         </div>
-                        <Link to="/login" className="text-sm text-blue-600 dark:text-blue-500 no-underline">
+                        <Link to="/login" className="text-sm text-gray-600 dark:text-gray-500 no-underline">
                             <AccountCircleIcon />Login
                         </Link>
-                    </div>
-                </div>
-            </nav>
-            <nav className="bg-gray-50 dark:bg-gray-700">
-                <div className="max-w-screen-xl px-4 py-3 mx-auto">
-                    <div className="flex items-center">
-                        <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
-                            <li>
-                                <Link
-                                    to="/"
-                                    className={`text-gray-900 dark:text-white no-underline ${location.pathname === '/' ? 'font-bold' : ''}`}
-                                    aria-current="page"
-                                >
-                                    Inicio
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/reportar"
-                                    className={`text-gray-900 dark:text-white no-underline ${location.pathname === '/reportar' ? 'font-bold' : ''}`}
-                                >
-                                    Reportar
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/recolectores"
-                                    className={`text-gray-900 dark:text-white no-underline ${location.pathname === '/recolectores' ? 'font-bold' : ''}`}
-                                >
-                                    Recolectores
-                                </Link>
-                            </li>
-                        </ul>
                     </div>
                 </div>
             </nav>
