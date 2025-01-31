67ff2fe9d71e2110d61c9b5483b3d9930fefc817
JuniorSD6650 <fancy6650@gmail.com>
2025-01-31 17:19:21 -0500

    chore: nombre opcional de bd arreglado

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,6 @@ module.exports = {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'gatilin_db'
+    database: process.env.DB_NAME || 'recolector_db'
   }
 };
