80c05687fd5e74cb8a966175e632fae1473e89d0
JuniorSD6650 <fancy6650@gmail.com>
2025-02-04 13:01:45 -0500

    fix: Correción de modelo de usuarios al registrar credenciales

--- createAdmin.js
+++ createAdmin.js
@@ -1,5 +1,3 @@
-// createAdmin.js
-
 require('dotenv').config();
 const bcrypt = require('bcryptjs');
 const knex = require('knex')(require('./knexfile.js'));
@@ -9,18 +7,34 @@ const createAdmin = async () => {
     const username = 'admin';
     const password = 'adminpassword';
 
-    const existingAdmin = await knex('usuarios').where({ username }).first();
+    // Verifica si ya existe un usuario con rol de 'admin'
+    const existingAdmin = await knex('usuarios').where({ role: 'admin' }).first();
     if (existingAdmin) {
       console.log('El usuario administrador ya existe.');
       return;
     }
 
+    // Crea el usuario en la tabla 'usuarios'
+    const result = await knex('usuarios').insert({
+      role: 'admin',
+      nombres: 'Admin',
+      primer_apellido: 'Admin',
+      email: 'admin@example.com',
+      telefono: '123456789',
+      created_at: knex.fn.now()
+    });
+
+    const userId = result[0] ? result[0] : result.insertId; // Obtiene el ID en MySQL
+
+    // Hashea la contraseña
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    await knex('usuarios').insert({
+    // Crea las credenciales asociadas al usuario
+    await knex('credenciales').insert({
       username: username,
       password: hashedPassword,
-      role: 'admin'
+      usuario_id: userId,
+      created_at: knex.fn.now()
     });
 
     console.log('Administrador creado exitosamente');
