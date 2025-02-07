d5c0bfc179fa233362afd434105c6404f5ed155d
JuniorSD6650 <fancy6650@gmail.com>
2025-02-07 11:58:27 -0500

    chore: transition - new library

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-// src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 import './App.css';
 import Home from './Home';
 import Login from './Login';
@@ -18,75 +18,96 @@ import Mapa from './Mapa';
 import Footer from './components/foot/Footer';
 import Navbar from './components/nav/Navbar';
 
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+        <Route path="/login" element={<PageWrapper><Login /></PageWrapper>} />
+
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <PageWrapper><AdminPage /></PageWrapper>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin/conductores"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <PageWrapper><AdminLocalizadorPage /></PageWrapper>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin/reportes"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <PageWrapper><AdminReportesPage /></PageWrapper>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin/vehiculos"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <PageWrapper><AdminVehiculosPage /></PageWrapper>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin/perfil"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <PageWrapper><AdminPerfilPage /></PageWrapper>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/localizador"
+          element={
+            <ProtectedRoute requiredRole="localizador">
+              <PageWrapper><LocalizadorPage /></PageWrapper>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="/reportar" element={<PageWrapper><Report /></PageWrapper>} />
+        <Route path="/recolectores" element={<PageWrapper><VehicleList /></PageWrapper>} />
+        <Route path="/recolectores/:id" element={<PageWrapper><VehicleDetail /></PageWrapper>} />
+        <Route path="/mapa" element={<PageWrapper><Mapa /></PageWrapper>} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+const PageWrapper = ({ children }) => (
+  <motion.div
+    initial={{ opacity: 0, x: -50 }}
+    animate={{ opacity: 1, x: 0 }}
+    exit={{ opacity: 0, x: 50 }}
+    transition={{ duration: 0.4 }}
+  >
+    {children}
+  </motion.div>
+);
+
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-
-            <Route
-              path="/admin"
-              element={
-                <ProtectedRoute requiredRole="admin">
-                  <AdminPage />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/admin/conductores"
-              element={
-                <ProtectedRoute requiredRole="admin">
-                  <AdminLocalizadorPage />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/admin/reportes"
-              element={
-                <ProtectedRoute requiredRole="admin">
-                  <AdminReportesPage />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/admin/vehiculos"
-              element={
-                <ProtectedRoute requiredRole="admin">
-                  <AdminVehiculosPage />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/admin/perfil"
-              element={
-                <ProtectedRoute requiredRole="admin">
-                  <AdminPerfilPage />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/localizador"
-              element={
-                <ProtectedRoute requiredRole="localizador">
-                  <LocalizadorPage />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route path="/reportar" element={<Report />} />
-            <Route path="/recolectores" element={<VehicleList />} />
-            <Route path="/recolectores/:id" element={<VehicleDetail />} />
-            <Route path="/mapa" element={<Mapa />} />
-          </Routes>
+          <AnimatedRoutes />
         </div>
         <Footer />
       </div>
