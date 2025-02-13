3c3b087fa258af443108494fe0678fe863e03e8c
ArvickJara <arvic2514@gmail.com>
2025-02-13 10:35:10 -0500

    feat: Update Home component styles and enhance layout

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -10,34 +10,26 @@ const Home = () => {
 
         <div className="min-h-screen flex items-center justify-center bg-gray-50">
             <div className="container mx-auto flex flex-col-reverse md:flex-row items-center px-6 md:px-12">
-                {/* Left Section */}
+                
                 <div className="text-center md:text-left md:max-w-lg">
-                    <h1 className="text-4xl md:text-5xl font-bold text-gray-800">
+                    <h1 className="text-4xl md:text-5xl font-bold" style={{ color: '#205287' }}>
                         Hola,
                     </h1>
-                    <h2 className="text-3xl md:text-4xl font-bold text-gray-800 mt-2">
+                    <h2 className="text-3xl md:text-4xl font-bold mt-2" style={{ color: '#205287' }}>
                         Recolector Digital de Amarilis
                     </h2>
-                    <p className="text-gray-600 mt-4">
+                    <p className="text-gray-600 mt-4 font-normal" style={{ color: '#595957' }}  >
                         Sistema de reporte y geolocalización de carros recolectores - podras ver exactamente donde se encuantra el carro lector y podras hacer un reporte de la basura.
                     </p>
                     <br />
                     <div className="flex space-x-4">
-                        <button className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600">
-                            Get Started →
+                        <button className="px-6 py-3 bg-green-custom text-white rounded-3xl shadow-lg hover:bg-green2-custom">    
+                            Get Started
                         </button>
-                        <button className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600">
-                            Get Started →
+                        <button className="px-6 py-3 bg-green-custom text-white rounded-3xl shadow-lg hover:green2-custom">
+                            Get Started
                         </button>
-                    </div>
-                </div>
-                {/* Right Section */}
-                <div className="md:w-1/2">
-                    <img
-                        src="/images/img-prueba.jpg"
-                        alt="Business Illustration"
-                        className="w-full"
-                    />
+                    </div> 
                 </div>
             </div>
         </div>
