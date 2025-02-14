138b573cac2852390a51f55158a3ff32aaa16900
ArvickJara <arvic2514@gmail.com>
2025-02-14 15:37:35 -0500

    feat: Enhance Home component with dynamic carousel and improved layout

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import CardAnimation from './components/CardAnimation/CardAnimation';
 import TituloConRegreso from './components/TituloConRegreso/TituloConRegreso';
 import { useNavigate} from 'react-router-dom';
+import { API_BASE_URL, BASE_URL } from './utils';
 
 const Home = () => {
 
+    const [publicaciones, setPublicaciones] = useState([]);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchPublicaciones = async () => {
+            try {
+                const response = await fetch(API_BASE_URL + 'publicaciones');
+                const data = await response.json();
+                setPublicaciones(data.publicaciones);
+            } catch (error) {
+                console.error('Error fetching publicaciones:', error);
+            }
+        };
+
+        fetchPublicaciones();
+    }, []);
 
     const handleRecolectoresClick = () => {
         navigate('/recolectores');
@@ -18,23 +34,38 @@ const Home = () => {
     };
 
     return (
-
-
         <div className="min-h-screen flex items-center justify-center bg-gray-50">
-            <div className="container mx-auto flex flex-col-reverse md:flex-row items-center px-6 md:px-12">
+            <div className="container mx-auto flex flex-col md:flex-row items-center px-6 md:px-20">
+                {/* Carousel Section */}
+                <div className="w-full md:w-1/2 md:order-2 mb-8 md:mb-0">
+                    <Carousel showThumbs={false} autoPlay infiniteLoop showArrows={false} swipeable emulateTouch>
+                        {publicaciones.map((publicacion) => (
+                            <div key={publicacion.id}>
+                                <img 
+                                    src={BASE_URL + `${publicacion.imagen_url}`}
+                                    alt={publicacion.titulo}
+                                    className="carousel-image"
+                                    draggable="false"
+                                    style={{ userSelect: 'none' }}
+                                />
+                            </div>
+                        ))}
+                    </Carousel>
+                </div>
                 
-                <div className="text-center md:text-left pr-16">
+                {/* Text Content Section */}
+                <div className="     md:text-left md:w-1/2 md:order-1 md:px-20">
                     <h1 className="text-4xl md:text-5xl font-bold" style={{ color: '#205287' }}>
-                        Hola,
+                        Hola Vecino,
                     </h1>
                     <h2 className="text-3xl md:text-4xl font-bold mt-2" style={{ color: '#205287' }}>
                         Recolector Digital de Amarilis
                     </h2>
-                    <p className="text-gray-600 mt-4 font-normal" style={{ color: '#595957' }}  >
+                    <p className="text-gray-600 mt-4 font-normal" style={{ color: '#595957' }}>
                         Sistema de reporte y geolocalización de carros recolectores - podras ver exactamente donde se encuantra el carro lector y podras hacer un reporte de la basura.
                     </p>
                     <br/>
-                    <div className="flex space-x-4">
+                    <div className="flex space-x-4  justify-start">
                         <button className="px-6 py-3 bg-green-custom text-white rounded-3xl shadow-lg hover:bg-green2-custom"
                             onClick={handleRecolectoresClick}>    
                             Recolectores
@@ -45,21 +76,6 @@ const Home = () => {
                         </button>
                     </div> 
                 </div>
-                
-            </div>
-            <div className="w-full md:px-12">
-                <Carousel showThumbs={false} autoPlay infiniteLoop showArrows={false} swipeable emulateTouch>
-                    <div>
-                        <img src="/images/img-prueba.png" alt="Noticia 1" className="carousel-image" draggable="false" style={{ userSelect: 'none' }} />
-                    </div>
-                    <div>
-                        <img src="/images/conciencia-ambienta2.jpg" alt="Aviso 1" className="carousel-image" draggable="false" style={{ userSelect: 'none' }} />
-                    </div>
-                    <div>
-                        <img src="/images/conciencia-ambienta3.jpg" alt="Comunicado 1" className="carousel-image" draggable="false" style={{ userSelect: 'none' }} />
-                    </div>
-                </Carousel>
-
             </div>
         </div>
     );
