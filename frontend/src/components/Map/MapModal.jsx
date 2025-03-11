c0681b6568894362bdc199eb99b172c61e95565c
JuniorSD6650 <fancy6650@gmail.com>
2025-03-11 11:35:37 -0500

    fix: Resuelto el problema del ícono predeterminado de Leaflet fusionando opciones de íconos personalizados

--- frontend/src/components/Map/MapModal.jsx
+++ frontend/src/components/Map/MapModal.jsx
@@ -7,6 +7,14 @@ import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
+// Fix the Leaflet default icon issue
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow
+});
+
 const MapModal = ({ ubicacion, onClose }) => {
 
     const mapRef = useRef(null);
