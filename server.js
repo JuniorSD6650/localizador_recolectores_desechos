cd8b19660e0c9949ad3e6cb6ed99c381d850aab6
Arvic Kenedi <arvic2514@gmail.com>
2025-07-09 14:23:54 -0500

    fix: Actualizar la configuración de CORS para usar el origen de desarrollo

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const routes = require('./routes');
 const app = express();
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'https://recolector.sistemasudh.com/',
+  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
