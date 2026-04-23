6e4e898f31742f9dcad15827fcb11fec7c1ed356
Edwin Junior Saavedra Dominguez <fancy6650@gmail.com>
2026-04-23 17:32:03 -0500

    fix: añadiendo slash (/) a BASEURL para arreglando el consumo de imagenes

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,6 +1,6 @@
 import Swal from 'sweetalert2';
 
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "http://localhost:3000/";
 export const API_BASE_URL = "http://localhost:3000/api/";
 
 export const parseDate = (dateString) => {
